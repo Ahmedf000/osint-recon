313be958f97f5cb14d5749e8fddaac7bb0038472
Ahmed <ahmedferid0@gmail.com>
2026-01-13 10:28:13 +0100

    Update person_profile.sh

--- person_profile.sh
+++ person_profile.sh
@@ -7,16 +7,16 @@ mkdir -p $OUTPUT_DIR
 
 echo "Creating profile for: $NAME"
 
-python osint.py -t "$NAME" -d 63 -o "$OUTPUT_DIR/linkedin.json"
-python osint.py -t "$NAME" -d 67 -o "$OUTPUT_DIR/github.json"
-python osint.py -t "$NAME" -d 90 -o "$OUTPUT_DIR/work_history.json"
+python3 osint.py -t "$NAME" -d 63 -o "$OUTPUT_DIR/linkedin.json"
+python3 osint.py -t "$NAME" -d 67 -o "$OUTPUT_DIR/github.json"
+python3 osint.py -t "$NAME" -d 90 -o "$OUTPUT_DIR/work_history.json"
 
-python osint.py -t "$NAME" -d 51 -o "$OUTPUT_DIR/resume.json"
-python osint.py -t "$NAME" -d 56 -o "$OUTPUT_DIR/papers.json"
+python3 osint.py -t "$NAME" -d 51 -o "$OUTPUT_DIR/resume.json"
+python3 osint.py -t "$NAME" -d 56 -o "$OUTPUT_DIR/papers.json"
 
-python osint.py -t "$NAME" -d 75 -o "$OUTPUT_DIR/email.json"
+python3 osint.py -t "$NAME" -d 75 -o "$OUTPUT_DIR/email.json"
 
-python osint.py -t "$NAME" -d 64 -o "$OUTPUT_DIR/twitter.json"
-python osint.py -t "$NAME" -d 65 -o "$OUTPUT_DIR/facebook.json"
+python3 osint.py -t "$NAME" -d 64 -o "$OUTPUT_DIR/twitter.json"
+python3 osint.py -t "$NAME" -d 65 -o "$OUTPUT_DIR/facebook.json"
 
 echo "Profile complete! Results in $OUTPUT_DIR/"
